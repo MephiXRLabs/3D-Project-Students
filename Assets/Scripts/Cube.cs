711ed987a8882c86c140f9801ab842d4a0817213
Victor <vic.baykov89@yandex.ru>
2023-03-22 15:35:55 +0300

    Transform movement

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -5,29 +5,24 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
+    [SerializeField]
+    private bool _shouldChangePosition;
 
-    [SerializeField] private GameObject _cube1;
-    [SerializeField] private MeshRenderer _renderer;
-
-    [SerializeField] private bool _shouldChangeColor;
-
-    [SerializeField] private Color _color;
+    [SerializeField] private float _speed;
 
     // Start is called before the first frame update
     private void Start()
     {
-        _cube1 = GameObject.FindGameObjectWithTag("Cube1");
-        _renderer = _cube1.GetComponent<MeshRenderer>();
+        
     }
     
     // Update is called once per frame
     private void Update()
     {
-        if (_shouldChangeColor)
+        if (_shouldChangePosition)
         {
-            //TODO Change color
-            _renderer.material.color = _color;
-            _shouldChangeColor = false;
+            transform.position += new Vector3(_speed * Time.deltaTime, 0, 0);
+            //_shouldChangePosition = false;
         }
 
         Debug.Log($"Update {gameObject.name}");
